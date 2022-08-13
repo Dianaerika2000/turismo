451ff4edf6d510de9d1cd3c21740facee0ed71a4
Eressea RS <rsilva@eresseasolutions.com>
2022-08-13 11:24:42 -0400

    Added component patito

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import Button from "../../components/Button/Button";
+import Patito from "../../components/Patito/Patito";
 export default function Home (props) {
   return (
     <div className="container">
       <div className="row">
         <div className="col my-3">
           <Button />
+          <Patito />
         </div>
       </div>
     </div>
