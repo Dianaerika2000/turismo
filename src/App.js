ed62e3d33da12557d4ab3a3c9952391a2434eaac
Eressea RS <rsilva@eresseasolutions.com>
2022-08-13 10:50:56 -0400

    Added bootstrap 5.2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/scss/stylesheet.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col my-3">
+            <button className="btn btn-primary">
+              Botón! &nbsp; <i className="bi bi-alarm"></i>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
