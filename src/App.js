f426fdbaa1249055de26cc387231c1edb2eb6151
Eressea RS <rsilva@eresseasolutions.com>
2022-08-13 11:00:38 -0400

    Added componentes and pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import './assets/scss/stylesheet.scss';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <div className="row">
-          <div className="col my-3">
-            <button className="btn btn-primary">
-              Botón! &nbsp; <i className="bi bi-alarm"></i>
-            </button>
-          </div>
-        </div>
-      </div>
+      <Home></Home>
     </div>
   );
 }
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import Button from "../../components/Button/Button";
+export default function Home (props) {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col my-3">
+          <Button />
+        </div>
+      </div>
+    </div>
+  );
+}
